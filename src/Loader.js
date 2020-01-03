ab2cd8f2baf3e9c75f3bce597ea753336edde87d
Akshay <akshay.a.mhatre@capgemini.com>
2020-01-03 18:57:49 +0530

    Codebase

---
+++ src/Loader.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Loader = props => {
+    console.log(props);
+    
+    return (
+        <div className="ui active dimmer">
+            <div className="ui text loader">{props.loadingText}</div>
+        </div>
+    );  
+};
+
+Loader.defaultProps = {
+    loadingText: 'Please Accept location request'
+}
+
+export default Loader
+
--- src/index.js
+++ src/index.js
@@ -1,12 +1,58 @@
+// Imports
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import SeasonDisplay from './SeasonDisplay';
+import Loader from './Loader';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Create class component
+class App extends React.Component {
+    state = {
+        lat: null,
+        errMessage: '',
+        loaderMessage: 'Allow the website to use location'
+    }
+    year = new Date()
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    componentDidMount() {
+        // Get user location
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({ lat: position.coords.latitude, errMessage: '' }),
+            err => this.setState({ errMessage: err.message })
+        );
+    }
+
+    // Render function
+    render() {
+        // Conditional - if geolocation api has error
+        if (this.state.errMessage) {
+            return (
+                <div>
+                    <h2>Error: {this.state.errMessage}</h2>
+                </div>
+            );
+        }
+        // Conditional - if geolocation api receives latitude
+        if (this.state.lat) {
+            return (
+                <div>
+                    <section style={{minHeight:'100vh', margin: '0 auto -60px'}}>
+                        <SeasonDisplay lat={this.state.lat} />
+                    </section>
+                    <footer style={{padding: '20px', backgroundColor: 'yellow'}}>
+                        Footer &copy; { this.year.getFullYear() }
+                    </footer>
+                </div>
+            );
+        }
+
+        // Loading screen till geolocation API receives response
+        return <Loader />
+
+    }
+}
+
+// Render
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
